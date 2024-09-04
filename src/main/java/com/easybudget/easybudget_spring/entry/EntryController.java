3aba261d82256294680e7fcb03dd2f9fd59b1532
Npvpr <nlmnever2@gmail.com>
2024-09-04 01:21:40 +0100

    CRUD and Exception for Entry and Category

--- src/main/java/com/easybudget/easybudget_spring/entry/EntryController.java
+++ src/main/java/com/easybudget/easybudget_spring/entry/EntryController.java
@@ -2,28 +2,47 @@ package com.easybudget.easybudget_spring.entry;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.easybudget.easybudget_spring.EasybudgetSpringApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
-@RequestMapping("/api/entries")
+@RequestMapping("/api/entry")
 public class EntryController {
 
     @Autowired
     private EntryService entryService;
 
-    // private static final Logger log =
-    // LoggerFactory.getLogger(EasybudgetSpringApplication.class);
+    @GetMapping("/all")
+    public List<Entry> getAllEntries() {
+        return entryService.getAllEntries();
+    }
 
-    @GetMapping
-    public List<Entry> getAllItems() {
-        return entryService.getAllItems();
+    @GetMapping("/{id}")
+    public Entry getEntryById(@PathVariable Long id) {
+        return entryService.getEntryById(id);
+    }
+
+    @PostMapping
+    public Entry addEntry(@RequestBody Entry entry) {
+        return entryService.addEntry(entry);
+    }
+
+    @PutMapping("/{id}")
+    public Entry updateEntry(@PathVariable Long id, @RequestBody Entry entry) {
+        return entryService.updateEntry(id, entry);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEntry(@PathVariable Long id) {
+        entryService.deleteEntry(id);
     }
 
 }
