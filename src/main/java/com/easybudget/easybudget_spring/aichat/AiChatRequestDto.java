2a4dbed94f47288ce67fe3496bfc0fd27b25037e
Npvpr <nlmnever2@gmail.com>
2025-09-04 16:31:36 +0100

    AiChat error fixed

--- src/main/java/com/easybudget/easybudget_spring/aichat/AiChatRequestDto.java
+++ src/main/java/com/easybudget/easybudget_spring/aichat/AiChatRequestDto.java
@@ -2,11 +2,15 @@ package com.easybudget.easybudget_spring.aichat;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AiChatRequestDto {
     @NotBlank(message = "Message cannot be blank.")
     @Size(min = 1, max = 250, message = "Message must be between 1 and 500 characters.")
